326d6ea6855d14f9c76ff3148667889772502663
Tech <tech@Techs-MacBook-Air.local>
2026-04-04 00:38:08 -0400

    Initial commit for Lab Test 2

---
+++ A2_iOS_james_100898394/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  A2_iOS_james_100898394
+//
+//  Created by Tech on 2026-04-04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
