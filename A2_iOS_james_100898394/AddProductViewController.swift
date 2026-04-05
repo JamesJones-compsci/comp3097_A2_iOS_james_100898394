63add24d639897485a1593aa30cdd1100860b49d
James Jones <jjsfitnesstoronto@gmail.com>
2026-04-05 14:26:31 -0400

    Next step - add screen functionality

---
+++ A2_iOS_james_100898394/AddProductViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AddProductViewController.swift
+//  A2_iOS_james_100898394
+//
+//  Created by Tech on 2026-04-05.
+//
+
+import UIKit
+
+class AddProductViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
